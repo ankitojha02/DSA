62d711ce007b4475e31736e914be0467d8745615
ankitojha02 <ojhaa2207@gmail.com>
2026-02-25 00:28:06 +0530

    adding naming convention

--- Variablebasics.java
+++ Variablebasics.java
@@ -9,5 +9,13 @@ public class Variablebasics {
         //System.out.println(x*y);
         x = 12; // Updation
         System.out.println(x);
+
+        // Variable name starts with Alphabets or _ or $
+        // Numbers are allowed but not in starting
+        // No special characters are allowed except _ or $
+        // No blank space in variable name
+        // Variable name must not be a keyword used in java
+        // Use simple, short and understandable name 
+        
     }
 }
