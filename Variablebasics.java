d00078f44fabcd3d63b5c08567921ea3c14200a7
ankitojha02 <ojhaa2207@gmail.com>
2026-02-24 23:29:12 +0530

    variable basics

---
+++ Variablebasics.java
@@ -0,0 +1,13 @@
+public class Variablebasics {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        System.out.println(x+20);
+        int y; // declaration
+        y = 15; // initialization
+        System.out.println(y);
+        //System.out.println(x*y);
+        x = 12; // Updation
+        System.out.println(x);
+    }
+}
