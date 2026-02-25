917e5104569cd99e4b6dd1606ac52bacdee9c2d8
ankitojha02 <ojhaa2207@gmail.com>
2026-02-25 23:11:52 +0530

    example input

--- Uses/input.java
+++ Uses/input.java
@@ -1,7 +1,19 @@
 package Uses;
 
+import java.util.Scanner;
+
 public class input {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // System.out.println("Enter radius");
+        // Scanner sc = new Scanner(System.in);
+        // double r = sc.nextDouble();
+        // double a = 3.14 * r * r;
+        // System.out.println(a);
+
+        System.out.print("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int sqaure = number * number;
+        System.out.println("Sqaure of number " + number + " is: " + sqaure);
     }
 }
