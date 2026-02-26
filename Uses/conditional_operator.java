0b20a06ad2df153779f7c4189d3d01f8ae28b5d4
ankitojha02 <ojhaa2207@gmail.com>
2026-02-27 00:08:25 +0530

    conditional operator

---
+++ Uses/conditional_operator.java
@@ -0,0 +1,18 @@
+package Uses;
+
+import java.util.Scanner;
+
+public class conditional_operator {
+    public static void main(String[] args) {
+        System.out.println("Enter any number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n%2==0) {
+            System.out.println("The number you entered is even number");
+        }
+        else{
+            System.out.println("The number you entered is odd number");
+        }
+    }
+}
