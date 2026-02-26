1cabc03d28b546fe3e156026e54f27fce157683e
ankitojha02 <ojhaa2207@gmail.com>
2026-02-26 23:25:21 +0530

    updated scenario of incre/decre operator

--- Uses/increment_decrement.java
+++ Uses/increment_decrement.java
@@ -3,10 +3,15 @@ package Uses;
 public class increment_decrement {
     public static void main(String[] args) {
         int ankit = 10;
-        System.out.println(ankit);
-        ankit = ankit + 1;
-        System.out.println(ankit);
-        ankit = ankit - 1;
-        System.out.println(ankit);
+        // Post increment
+       System.out.println(ankit++);
+       System.out.println(ankit);
+       // Pre increment
+       System.out.println(++ankit);
+
+       // Special scenario
+       int x = 10;
+       int y = x++;
+       System.out.println(x+" "+y); // Output : 11 10
     }
 }
