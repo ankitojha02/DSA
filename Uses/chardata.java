e261afa246b3bdd45ccf21623645b386df184398
ankitojha02 <ojhaa2207@gmail.com>
2026-02-26 23:09:28 +0530

    inrement decrement

--- Uses/chardata.java
+++ Uses/chardata.java
@@ -17,5 +17,13 @@ public class chardata {
         //type casting - explicit
         int y = (int)b;
         System.out.println(y);
+
+        //char + int/double = int/double
+        System.out.println(b+0);
+
+       //Integer to character
+       int g = 97;
+       char res = (char)g;
+       System.out.println(res);
     }
 }
---
+++ Uses/increment_decrement.java
@@ -0,0 +1,12 @@
+package Uses;
+
+public class increment_decrement {
+    public static void main(String[] args) {
+        int ankit = 10;
+        System.out.println(ankit);
+        ankit = ankit + 1;
+        System.out.println(ankit);
+        ankit = ankit - 1;
+        System.out.println(ankit);
+    }
+}
