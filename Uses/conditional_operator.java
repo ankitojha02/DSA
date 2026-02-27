ff06abfb9d0f8e5e848efd0fe853440502478e4e
ankitojha02 <ojhaa2207@gmail.com>
2026-02-27 22:43:31 +0530

    adding questions

--- Uses/conditional_operator.java
+++ Uses/conditional_operator.java
@@ -8,11 +8,19 @@ public class conditional_operator {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if (n%2==0) {
-            System.out.println("The number you entered is even number");
+        // if (n%2==0) {
+        //     System.out.println("The number you entered is even number");
+        // }
+        // else{
+        //     System.out.println("The number you entered is odd number");
+        // }
+
+        if (n < 0) {
+           int result = n * (-1);
+            System.out.println("Absolute value of the number is : " + " " + result);
         }
         else{
-            System.out.println("The number you entered is odd number");
+            System.out.println("Absolute value of the number is : " + n) ;
         }
     }
 }
