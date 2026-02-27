8b6174038ad38e28a9ad2e61a9d871249be0f0cc
ankitojha02 <ojhaa2207@gmail.com>
2026-02-27 23:39:31 +0530

    solving more question

--- Uses/conditional_operator.java
+++ Uses/conditional_operator.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class conditional_operator {
     public static void main(String[] args) {
-        System.out.println("Enter any number");
+        System.out.println("Enter cost price");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        double cp = sc.nextDouble();
+        System.out.println("Enter selling price");
+        double sp = sc.nextDouble();
+        double result;
 
         // if (n%2==0) {
         //     System.out.println("The number you entered is even number");
@@ -14,13 +17,45 @@ public class conditional_operator {
         // else{
         //     System.out.println("The number you entered is odd number");
         // }
+        
+        // Absolute value
+        // if (n < 0) {
+        //    int result = n * (-1);
+        //     System.out.println("Absolute value of the number is : " + " " + result);
+        // }
+        // else{
+        //     System.out.println("Absolute value of the number is : " + n) ;
+        // }
 
-        if (n < 0) {
-           int result = n * (-1);
-            System.out.println("Absolute value of the number is : " + " " + result);
+
+        // Check whether the number is perfect interger or in decimal
+        // if (n<0) {
+        //     n = -n;
+        // }
+        // // Typecasting : Convert floating value into integer value
+        // int x = (int)n;
+        
+
+        // if(n-x > 0){
+        //    System.out.println("The number you entered is decimal");
+        // }
+        // else{
+        //     System.out.println("The number you entered is perfect integer");
+        // }
+
+        //If cost price and selling price of an item is input 
+        //through the keyboard, write a program to determine whether 
+        //the seller has made profit or incurred loss or no profit
+        //no loss. Also determine how much profit he made or loss
+        //he incurred.
+        if(cp>sp){
+            result = cp - sp;
+            System.out.println("You made a loss of : " + result);
         }
         else{
-            System.out.println("Absolute value of the number is : " + n) ;
+            result = sp-cp;
+            System.out.println("You made a profit of : " + result);
         }
+
     }
 }
