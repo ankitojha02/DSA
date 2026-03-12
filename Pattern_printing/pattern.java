c10524b53a0cdcd7d0f013e84442eda1851fac0e
ankitojha02 <ojhaa2207@gmail.com>
2026-03-12 23:34:04 +0530

    solving 1st question

--- Pattern_printing/pattern.java
+++ Pattern_printing/pattern.java
@@ -7,12 +7,20 @@ public class pattern {
         System.out.println("Enter the number of rows");
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
-        System.out.println("Enter the number of columns");
-        int cols = sc.nextInt();
+        // System.out.println("Enter the number of columns");
+        // int cols = sc.nextInt();
+        
+        //Stars rectangle
+        // for(int i = 1; i<=rows; i++){ // Kitni line hogi
+        //     for(int j = 1; j<=cols; j++){ // Har line me kitne stars honge
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
 
-        for(int i = 1; i<=rows; i++){ // Kitni line hogi
-            for(int j = 1; j<=cols; j++){ // Har line me kitne stars honge
-                System.out.print(" * ");
+        for(int i =1 ; i<=rows ; i++){
+            for(int j = 1; j<=rows; j++){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
