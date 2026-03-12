f77fe3cfc614930962c41a5d8f8765f3055abcdc
ankitojha02 <ojhaa2207@gmail.com>
2026-03-12 22:53:07 +0530

    solving rectangular stars

--- Pattern_printing/pattern.java
+++ Pattern_printing/pattern.java
@@ -1,7 +1,20 @@
 package Pattern_printing;
 
+import java.util.Scanner;
+
 public class pattern {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Enter the number of rows");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of columns");
+        int cols = sc.nextInt();
+
+        for(int i = 1; i<=rows; i++){ // Kitni line hogi
+            for(int j = 1; j<=cols; j++){ // Har line me kitne stars honge
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
     }
 }
