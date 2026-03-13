9ab2806b26a9d1e4bdb946220480eaaac2509d9b
ankitojha02 <ojhaa2207@gmail.com>
2026-03-13 22:24:30 +0530

    solving more problems

--- Pattern_printing/pattern.java
+++ Pattern_printing/pattern.java
@@ -2,7 +2,6 @@ package Pattern_printing;
 
 import java.util.Scanner;
 
-
 public class pattern {
     public static void main(String[] args) {
         System.out.println("Enter the number of rows");
@@ -10,115 +9,131 @@ public class pattern {
         int rows = sc.nextInt();
         // System.out.println("Enter the number of columns");
         // int cols = sc.nextInt();
-        
-        //Stars rectangle
+
+        // Stars rectangle
         // for(int i = 1; i<=rows; i++){ // Kitni line hogi
-        //     for(int j = 1; j<=cols; j++){ // Har line me kitne stars honge
-        //         System.out.print(" * ");
-        //     }
-        //     System.out.println();
+        // for(int j = 1; j<=cols; j++){ // Har line me kitne stars honge
+        // System.out.print(" * ");
         // }
-        
-        /*Printing : 123
-                     123
-                     123
+        // System.out.println();
+        // }
 
-        */
+        /*
+         * Printing : 123
+         * 123
+         * 123
+         * 
+         */
         // for(int i =1 ; i<=rows ; i++){
-        //     for(int j = 1; j<=rows; j++){
-        //         System.out.print(j + " ");
-        //     }
-        //     System.out.println();
+        // for(int j = 1; j<=rows; j++){
+        // System.out.print(j + " ");
         // }
-        
+        // System.out.println();
+        // }
+        // Alphabets rectangle
+        // for(int i =1 ; i<=rows ; i++){
+        // int k = 65;
+        // for(int j = 1; j<=rows; j++){
 
-        //Alphabets rectangle
-        //  for(int i =1 ; i<=rows ; i++){
-        //     int k = 65;
-        //     for(int j = 1; j<=rows; j++){
-                
-        //         System.out.print((char)k + " ");
-        //         k= k + 1;
-        //     }
-        //     System.out.println();
+        // System.out.print((char)k + " ");
+        // k= k + 1;
         // }
-        
-        //Printing : 1111/n 2222/n 3333/n 4444/n
+        // System.out.println();
+        // }
+
+        // Printing : 1111/n 2222/n 3333/n 4444/n
         // for(int i = 1; i<=rows; i++){
-        //     for(int j = 1; j<=rows;j++){
-        //         System.out.print(i + " ");
-        //     }
-        //     System.out.println();
+        // for(int j = 1; j<=rows;j++){
+        // System.out.print(i + " ");
+        // }
+        // System.out.println();
         // }
 
-        //Printing : AAAA/n BBBB/n CCCC/n DDDD/n
+        // Printing : AAAA/n BBBB/n CCCC/n DDDD/n
         // int a = 65;
         // for(int i = 1; i<=rows; i++){
-        //     for(int j = 1; j<=rows; j++){
-        //        System.out.print((char)a + " ");
-        //     }
-        //     System.out.println();
-        //     a = a+1;
+        // for(int j = 1; j<=rows; j++){
+        // System.out.print((char)a + " ");
         // }
-        
-        // Printing : aaaa/n BBBB/n cccc/n DDDD/n
-       
+        // System.out.println();
+        // a = a+1;
+        // }
 
-        //  
+        // Printing : aaaa/n BBBB/n cccc/n DDDD/n
+
         
+
         // Printing left perpendicular right angle triangle of stars
         // for(int i = 1; i<=rows; i++){
-        //     for(int j = 1; j <= i; j++){
-        //         System.out.print(j + " ");
-        //     }
-        //     System.out.println();
+        // for(int j = 1; j <= i; j++){
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
         // }
 
-        //Printing A/n AB/n ABC/n ABCD/n
+        // Printing A/n AB/n ABC/n ABCD/n
 
         // for(int i = 1; i<=rows; i++){
-        //     for(int j= 1; j<=i; j++){
-        //         System.out.print((char)(j+64) + " ");
-        //     }
-        //     System.out.println();
+        // for(int j= 1; j<=i; j++){
+        // System.out.print((char)(j+64) + " ");
+        // }
+        // System.out.println();
         // }
 
-        //Printing A/n BB/n CCC/n DDDD/n
+        // Printing A/n BB/n CCC/n DDDD/n
         // int k = 65;
         // for(int i = 1; i<=rows; i++){
-        //     for(int j = 1; j<=i; j++){
-        //         System.out.print((char)k + " ");
-        //     }
-        //     System.out.println();
-        //     k++;
+        // for(int j = 1; j<=i; j++){
+        // System.out.print((char)k + " ");
         // }
-        
-        
-        //Printing : 1/n AB/n 2/n ABC/n 1234
+        // System.out.println();
+        // k++;
+        // }
+
+        // Printing : 1/n AB/n 2/n ABC/n 1234
         // for(int i = 1; i <= rows; i++){
-           
-            
-        //     if(i%2!=0){
-        //          for(int j =1 ; j<= i; j++){
-        //             System.out.print(j + " ");
-        //          }
-        //     }
 
-        //     else{
-        //         for(int j =1; j<=i ; j++){
-        //             System.out.print((char)(j+64) + " ");
-        //         }
-        //     }
-           
-        //     System.out.println();
-           
+        // if(i%2!=0){
+        // for(int j =1 ; j<= i; j++){
+        // System.out.print(j + " ");
+        // }
         // }
 
-        for(int i = rows; i>=1; i--){
-            for(int j = 1; j<=i; j++){
-                System.out.print("*"+ " ");
-            }
-            System.out.println();
-        }
+        // else{
+        // for(int j =1; j<=i ; j++){
+        // System.out.print((char)(j+64) + " ");
+        // }
+        // }
+
+        // System.out.println();
+
+        // }
+
+        // Printing : ****/n ***/n **/n *
+        // for (int i = rows; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) { 
+        //         System.out.print("*" + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Best template for above problem
+        // for(int i = 1; i<=rows; i++){
+        //       for(int j = 1; j<=rows+1-i; j++){
+        //         System.out.print(j + " ");
+        //       }
+        //       System.out.println();
+        // }
+
+        //Print the given pattern : abcd/n abc/n ab/n a
+        // for(int i = 1; i <=rows; i++){
+        //     for(int j = 1 ; j<=rows+1-i; j++){
+        //         System.out.print((char)(j+96) + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        
+
     }
 }
