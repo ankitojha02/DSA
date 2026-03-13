a0b3095139a865ffee8170d744851adf1fc7ba62
ankitojha02 <ojhaa2207@gmail.com>
2026-03-13 20:33:13 +0530

    printing some patterns of alphabets

--- Pattern_printing/pattern.java
+++ Pattern_printing/pattern.java
@@ -2,6 +2,8 @@ package Pattern_printing;
 
 import java.util.Scanner;
 
+import Uses.chardata;
+
 public class pattern {
     public static void main(String[] args) {
         System.out.println("Enter the number of rows");
@@ -41,14 +43,45 @@ public class pattern {
         //     }
         //     System.out.println();
         // }
+        
+        //Printing : 1111/n 2222/n 3333/n 4444/n
+        // for(int i = 1; i<=rows; i++){
+        //     for(int j = 1; j<=rows;j++){
+        //         System.out.print(i + " ");
+        //     }
+        //     System.out.println();
+        // }
 
-        for(int i = 1; i<=rows; i++){
-            for(int j = 1; j<=rows;j++){
-                System.out.print(i + " ");
+        //Printing : AAAA/n BBBB/n CCCC/n DDDD/n
+        // int a = 65;
+        // for(int i = 1; i<=rows; i++){
+        //     for(int j = 1; j<=rows; j++){
+        //        System.out.print((char)a + " ");
+        //     }
+        //     System.out.println();
+        //     a = a+1;
+        // }
+        
+        // Printing : aaaa/n BBBB/n cccc/n DDDD/n
+       
+
+         char ch = 'a';
+
+         for(int i = 1; i<=rows; i++){
+            char printChar;
+
+            if (i % 2 ==0) {
+                printChar = (char)(ch - 32);// convert to uppercase
+            } else{
+                printChar = ch;
+            }
+
+            for(int j = 1; j<=4; j++){
+                System.out.print(printChar + " ");
             }
             System.out.println();
-        }
-
+            ch++;
+         }
 
     }
 }
