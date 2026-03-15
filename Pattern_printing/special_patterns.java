aaaf225654ae0aedc82ac18dfd607f905e112c03
ankitojha02 <ojhaa2207@gmail.com>
2026-03-15 21:20:49 +0530

    Printing Diamond pattern

--- Pattern_printing/special_patterns.java
+++ Pattern_printing/special_patterns.java
@@ -199,18 +199,47 @@ public class special_patterns {
         // }
 
         //Printing pyramid 
-        for(int i = 1; i<=rows; i++){
-            for(int j = 1; j<=rows + 1-i; j++){
+        // for(int i = 1; i<=rows; i++){
+        //     for(int j = 1; j<=rows + 1-i; j++){
+        //         System.out.print("  ");
+        //     }
+            
+        //     for(int j = 1; j<=2*i-1; j++){
+                
+        //         System.out.print("* ");
+             
+        //     }
+        //     System.out.println();
+        // }
+
+        // Printing Diamond stars
+        // Upper part
+        for(int i = 1; i <= rows; i++){
+
+            for(int j = 1; j <= rows - i; j++){
                 System.out.print("  ");
             }
-            
-            for(int j = 1; j<=2*i-1; j++){
-                
+
+            for(int j = 1; j <= 2*i - 1; j++){
                 System.out.print("* ");
-             
             }
+
             System.out.println();
         }
+        // Lower part
+        for(int i = rows-1; i >= 1; i--){
+
+            for(int j = 1; j <= rows - i; j++){
+                System.out.print("  ");
+            }
+
+            for(int j = 1; j <= 2*i - 1; j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
 
     }
 }
