ceadd0a75d150166193f72366a5359c0d827fdc7
ankitojha02 <ojhaa2207@gmail.com>
2026-03-16 22:35:25 +0530

    solving maximum of three numbers

--- Methods/BuiltIn.java
+++ Methods/BuiltIn.java
@@ -4,12 +4,20 @@ public class BuiltIn {
     public static void main(String[] args) {
         //Maths Built-in functions
 
-        System.out.println(Math.sqrt(9));
-        System.out.println(Math.cbrt(27));
-        System.out.println(Math.ceil(90.5));
-        System.out.println(Math.abs(-40));
-        System.out.println(Math.floor(6.8));
-        System.out.println(Math.min(5, 5.8));
-        System.out.println(Math.max(5, 5.8));
+        // System.out.println(Math.sqrt(9));
+        // System.out.println(Math.cbrt(27));
+        // System.out.println(Math.ceil(90.5));
+        // System.out.println(Math.abs(-40));
+        // System.out.println(Math.floor(6.8));
+        // System.out.println(Math.min(5, 5.8));
+        // System.out.println(Math.max(5, 5.8));
+        // System.out.println(Math.pow(5, 2));
+        
+        // Find the maximum of three numbers using built-in function
+        int a = 15; 
+        int b = 9;
+        int c = 8;
+        System.out.println(Math.max(Math.max(a, b),Math.max(c, b)));
+
     }
 }
