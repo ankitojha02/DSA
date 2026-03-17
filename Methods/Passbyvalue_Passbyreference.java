506dd541a82c9bea41c56091dd23a54bb9b128ea
ankitojha02 <ojhaa2207@gmail.com>
2026-03-17 23:56:49 +0530

    solving

--- Methods/Passbyvalue_Passbyreference.java
+++ Methods/Passbyvalue_Passbyreference.java
@@ -3,10 +3,10 @@ package Methods;
 public class Passbyvalue_Passbyreference {
 
     public static void change(int x){
-        x = 10;
+        x = 10;  //This x is local for only change method
     }
     public static void main(String[] args) {
-        int x = 6;
+        int x = 6; //This x is only for main method
         System.out.println(x);
         change(x);
         System.out.println(x); // Output: 6/n 6
