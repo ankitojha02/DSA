a2075e19cf823f7b4c3c11e34d8131dc63000a06
ankitojha02 <ojhaa2207@gmail.com>
2026-03-22 23:54:03 +0530

    Printing sum of elements of an array

--- Arrays/Output_Input_Array.java
+++ Arrays/Output_Input_Array.java
@@ -29,18 +29,38 @@ public class Output_Input_Array {
 
 
         // Question : Take array as input from the user and print negative numbers only
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[5];
+        // int[] arr = new int[5];
+        // for(int i = 0; i<arr.length; i++){
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // for(int i=0; i<arr.length; i++){
+        //     if(arr[i] < 0){
+        //         System.out.print(arr[i] + " ");
+        //     }
+        // }
+   
+        //You can also take array lenght as input from the user using Scanner class
+
+
+        //Print sum of elements of the array
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size");
+        int n = sc.nextInt();
+
+        System.out.println("Enter elements of an array");
+        int[] arr = new int[n];
         for(int i = 0; i<arr.length; i++){
             arr[i] = sc.nextInt();
         }
 
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] < 0){
-                System.out.print(arr[i] + " ");
-            }
+        for(int i = 0; i<arr.length; i++){
+            sum = sum + arr[i];
         }
+        System.out.println("Sum of the given array is: " + sum);
 
 
     }
