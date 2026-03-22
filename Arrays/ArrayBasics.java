daddc64ee759b1b9e9d34ab4310c990f75ed67e4
ankitojha02 <ojhaa2207@gmail.com>
2026-03-22 22:27:18 +0530

    learning arrays

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+        int[] x = {6, 19, 7, 69, 9, 171, 5};
+        //Indexing
+
+        System.out.println(x[2]);
+    }
+}
