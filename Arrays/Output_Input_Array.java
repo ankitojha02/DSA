ff36aa2a75d9bb2e07d0fd0f28609d82833570da
ankitojha02 <ojhaa2207@gmail.com>
2026-03-22 23:26:27 +0530

    Question : Take array as input from the user and print negative numbers only

--- Arrays/Output_Input_Array.java
+++ Arrays/Output_Input_Array.java
@@ -11,22 +11,36 @@ public class Output_Input_Array {
         // }
 
         //Input array from user
-        int[] arr = new int[7]; //default value of empty array is 0
+        // int[] arr = new int[7]; //default value of empty array is 0
 
+        // Scanner sc = new Scanner(System.in);
+
+        // for(int i = 0; i<arr.length; i++){
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // // System.out.println(arr[4]);
+
+        // for(int i = 0; i<arr.length; i++){
+        //     // System.out.print(arr[i]+ " ");
+        //     // System.out.println();
+        //     System.out.print(arr[i] * 2); // For doubling the value of elements in an array
+        // }
+
+
+        // Question : Take array as input from the user and print negative numbers only
         Scanner sc = new Scanner(System.in);
 
+        int[] arr = new int[5];
         for(int i = 0; i<arr.length; i++){
             arr[i] = sc.nextInt();
         }
 
-        // System.out.println(arr[4]);
-
-        for(int i = 0; i<arr.length; i++){
-            // System.out.print(arr[i]+ " ");
-            // System.out.println();
-            System.out.print(arr[i] * 2); // For doubling the value of elements in an array
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] < 0){
+                System.out.print(arr[i] + " ");
+            }
         }
-
 
 
     }
