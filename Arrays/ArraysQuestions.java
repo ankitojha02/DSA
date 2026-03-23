e7e97b9bcada8591fb0803e058274606649a8447
ankitojha02 <ojhaa2207@gmail.com>
2026-03-24 00:11:32 +0530

    product of an array

--- Arrays/ArraysQuestions.java
+++ Arrays/ArraysQuestions.java
@@ -4,43 +4,53 @@ import java.util.Scanner;
 
 public class ArraysQuestions {
     public static void main(String[] args) {
-        //Multiply odd indexed elements by 2 and add 10 to even indexed elements
         
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of an array");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        
+
         System.out.println("Enter elements of an array");
-        for(int i = 0; i< arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         print(arr);
     }
 
+    // Multiply odd indexed elements by 2 and add 10 to even indexed elements
     // public static void print(int[] arr){
-    //     for(int i =0; i<arr.length; i++){
-    //         if(i%2==0){
-    //             System.out.print(arr[i] + 10 + " ");
+    // for(int i =0; i<arr.length; i++){
+    // if(i%2==0){
+    // System.out.print(arr[i] + 10 + " ");
+    // }
+    // else{
+    // System.out.print(arr[i] * 2 + " ");
+    // }
+    // }
+    // }
+
+    // Search whether the given number present in an array or not
+    // public static void print(int[] arr) {
+    //     System.out.println("Enter the number you want to search");
+    //     Scanner sc = new Scanner(System.in);
+    //     int number = sc.nextInt();
+
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (arr[i] == number) {
+    //             System.out.print("Yes this number is present in the array" + "  " + arr[i]);
     //         }
-    //         else{
-    //             System.out.print(arr[i] * 2 + " ");
-    //         }
+
     //     }
     // }
 
+    //Product of Array
+
     public static void print(int[] arr){
-        System.out.println("Enter the number you want to search");
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-
+        int product = 1;
         for(int i=0; i<arr.length; i++){
-            if(arr[i] == number){
-                System.out.print("Yes this number is present in the array" + "  " + arr[i]);
-            }
-           
+          product = product * arr[i];
         }
+        System.out.println(product);
     }
-
-
 }
