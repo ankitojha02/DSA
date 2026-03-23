a123c6d7a7e530cda773af6fb748147838f60496
ankitojha02 <ojhaa2207@gmail.com>
2026-03-23 23:22:08 +0530

    Deep copy of arrays

--- Arrays/ShallowCopyDeepCopy.java
+++ Arrays/ShallowCopyDeepCopy.java
@@ -1,12 +1,18 @@
 package Arrays;
-
+import java.util.Arrays;
 public class ShallowCopyDeepCopy {
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,50};
-        int[] x = arr; // x is shallow copy of array
+        // int[] x = arr; // x is shallow copy of array i.e., x is a copy of arr
 
-        for(int i=0; i<x.length; i++){
-            System.out.println(x[i]);
-        }
+        // for(int i=0; i<x.length; i++){
+        //     System.out.println(x[i]);
+        // }
+
+        int[] deep = Arrays.copyOf(arr, arr.length); //deep copy
+        deep[0] = 100;
+        System.out.println(arr[0]);
+        System.out.println(deep[0]);
+
     }
 }
