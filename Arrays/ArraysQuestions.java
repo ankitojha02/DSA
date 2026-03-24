9ffc0854d5ae216c6b1147c5d8b6d36b9e845105
ankitojha02 <ojhaa2207@gmail.com>
2026-03-24 23:19:29 +0530

    printing second largest element in an array

--- Arrays/ArraysQuestions.java
+++ Arrays/ArraysQuestions.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class ArraysQuestions {
     public static void main(String[] args) {
-        
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of an array");
@@ -32,49 +31,72 @@ public class ArraysQuestions {
 
     // Search whether the given number present in an array or not
     // public static void print(int[] arr) {
-    //     System.out.println("Enter the number you want to search");
-    //     Scanner sc = new Scanner(System.in);
-    //     int number = sc.nextInt();
+    // System.out.println("Enter the number you want to search");
+    // Scanner sc = new Scanner(System.in);
+    // int number = sc.nextInt();
 
-    //     for (int i = 0; i < arr.length; i++) {
-    //         if (arr[i] == number) {
-    //             System.out.print("Yes this number is present in the array" + "  " + arr[i]);
-    //         break;
-    //}
-
-    //     }
+    // for (int i = 0; i < arr.length; i++) {
+    // if (arr[i] == number) {
+    // System.out.print("Yes this number is present in the array" + " " + arr[i]);
+    // break;
     // }
 
-    //Product of Array
+    // }
+    // }
+
+    // Product of Array
 
     // public static void print(int[] arr){
-    //     int product = 1;
-    //     for(int i=0; i<arr.length; i++){
-    //       product = product * arr[i];
-    //     }
-    //     System.out.println(product);
+    // int product = 1;
+    // for(int i=0; i<arr.length; i++){
+    // product = product * arr[i];
+    // }
+    // System.out.println(product);
     // }
 
-    //Two sum
-    public static void print(int[] arr){
-       int target = 2;
-       boolean achieved = false;
-       for(int i = 0; i<arr.length; i++){
-        for(int j=0; j<arr.length; j++){
-            if(arr[i] + arr[j] == target && i != j){
-                System.out.println("The target is achieved by two numbers: " + arr[i] + " and " + arr[j] + " at index: " + i + " and " + j);
-                achieved = true;
-              
-            }
-            if(achieved == true){
-                break;
-            }
-            
-        }
-       }
+    // Two sum
+    // public static void print(int[] arr){
+    // int target = 16;
+    // boolean achieved = false;
+    // for(int i = 0; i<arr.length; i++){
+    // for(int j=i+1; j<arr.length; j++){
+    // if(arr[i] + arr[j] == target ){
+    // System.out.println("The target is achieved by two numbers: " + arr[i] + " and
+    // " + arr[j] + " at index: " + i + " and " + j);
+    // achieved = true;
 
-       if(achieved == false){
-        System.out.println("Target not achieved");
-       }
+    // }
+    // if(achieved == true){
+    // break;
+    // }
+
+    // }
+    // }
+
+    // if(achieved == false){
+    // System.out.println("Target not achieved");
+    // }
+    // }
+
+    // Print the second maximum element in the array
+
+    public static void print(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        int secondMax = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] != max ){
+               if(arr[i]>secondMax){
+                secondMax = arr[i];
+               }
+            }
+        }
+
+        System.out.println("The second largest element in an array is: "+ secondMax);
     }
+
 }
