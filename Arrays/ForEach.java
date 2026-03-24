f58ca43b3353c070cf102b6e40e2e3c9a3c554e5
ankitojha02 <ojhaa2207@gmail.com>
2026-03-24 23:43:00 +0530

    We can't update element in an array using for each loop as it makes the copy of that array

--- Arrays/ForEach.java
+++ Arrays/ForEach.java
@@ -5,8 +5,9 @@ public class ForEach {
         int[] arr = {5,8,2,6,9};
 
         //Syntax
-        for(int ele : arr){
+        for(int ele : arr){ //element in array 
             System.out.println(ele);
-        }
+        } 
+        // We can't update element in an array using for each loop as it makes the copy of that array
     }
 }
