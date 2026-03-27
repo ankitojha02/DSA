d9fe22c1b1d884db5d9390770f10047a9b312c97
ankitojha02 <ojhaa2207@gmail.com>
2026-03-27 22:18:21 +0530

    starting Array list basic

---
+++ Arrays/ArrayListsBasic.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+import java.util.ArrayList;
+public class ArrayListsBasic {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(25);
+        arr.add(21);
+        arr.add(18);
+        arr.add(29);
+        arr.add(10);
+
+        System.out.println(arr.get(2));
+    }
+}
