a0b7ed89b26fae9e472d0142b1e10014f0feeded
ankitojha02 <ojhaa2207@gmail.com>
2026-03-30 23:07:40 +0530

    different approach to find duplicate element in an array

--- TimeSpaceComplexity/tandscomplexity.java
+++ TimeSpaceComplexity/tandscomplexity.java
@@ -8,12 +8,31 @@ public class tandscomplexity {
 
         // First Approach
 
-        int[] arr = {5,1,3,4,2,3};
+        // int[] arr = {5,1,3,4,2,3};
 
-        for(int i =0; i<arr.length; i++){
-            for(int j = i+1; j<arr.length;j++){
-                if(arr[i]==arr[j])
-                System.out.println("Duplicate element is : " + arr[i]);
+        // for(int i =0; i<arr.length; i++){
+        //     for(int j = i+1; j<arr.length;j++){
+        //         if(arr[i]==arr[j])
+        //         System.out.println("Duplicate element is : " + arr[i]);
+        //     }
+        // }
+        
+        // In this approach , the max number of operations to be performed is : 5+4+3+2 = 14
+
+
+        // Second Approach
+
+        int[] arr = {5,1,6,7,6,2,3};
+
+        boolean[] flag = new boolean[arr.length+1];
+        
+        for(int i=0; i<arr.length; i++){
+            int ele = arr[i];
+            if(flag[ele] == true){
+                System.out.println("Duplicate element is : " + ele);
+            }
+            else{
+                flag[ele] = true;
             }
         }
     }
