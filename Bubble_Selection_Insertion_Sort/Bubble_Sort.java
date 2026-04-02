1238d937dea2ee8574e37fb349a97ae9f919e34a
ankitojha02 <ojhaa2207@gmail.com>
2026-04-02 23:22:55 +0530

    Bubble Sort : An algorithm that sorts array - Swap adjacent elements

--- Bubble_Selection_Insertion_Sort/Bubble_Sort.java
+++ Bubble_Selection_Insertion_Sort/Bubble_Sort.java
@@ -4,26 +4,35 @@ public class Bubble_Sort {
     public static void main(String[] args) {
         // Bubble Sort : An algorithm that sorts array - Swap adjacent elements
 
-        int[] arr= {5, -2, 6,7, 2, 0, 7 , 2};
+        int[] arr = { 5, -2, 6, 7, 2, 0, 7, 2 };
         int n = arr.length;
 
         print(arr);
+        for (int i = 0; i < n - 1; i++) {
+            boolean isSorted = true;
 
-        for(int j = 0; j<n-1; j++){
-            for(int i=0; i<n-1; i++){
-                if(arr[i]>arr[i+1]){
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    isSorted = false;
                 }
             }
+
+            // If no swaps happened → already sorted
+            if (isSorted)
+                break;
         }
+
         System.out.println();
         print(arr);
     }
 
-    public static void print(int[] arr){
-        for(int ele : arr){
+    public static void print(int[] arr) {
+        for (int ele : arr) {
             System.out.print(ele + " ");
         }
     }
