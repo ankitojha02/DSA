6f652482bfd92be9e4412ed67aa061449a8004ff
ankitojha02 <ojhaa2207@gmail.com>
2026-04-05 19:55:48 +0530

    Selection sort is unstable as compared to Bubble sort

--- Bubble_Selection_Insertion_Sort/Bubble_Sort.java
+++ Bubble_Selection_Insertion_Sort/Bubble_Sort.java
@@ -4,30 +4,31 @@ public class Bubble_Sort {
     public static void main(String[] args) {
         // Bubble Sort : An algorithm that sorts array - Swap adjacent elements
 
-        int[] arr = { 5, -2, 6, 7, 2, 0, 7, 2, 0, 0 };
+        // int[] arr = { 5, -2, 6, 7, 2, 0, 7, 2, 0, 0 };
+        int[] arr = {7,3,4,7,8,1};
         int n = arr.length;
 
         print(arr);
 
         // Bubble sorting in ascending order
-        // for (int i = 0; i < n - 1; i++) {
-        //     boolean isSorted = true;
+        for (int i = 0; i < n - 1; i++) {
+            boolean isSorted = true;
 
-        //     for (int j = 0; j < n - 1 - i; j++) {
-        //         if (arr[j] > arr[j + 1]) {
-        //             // swap
-        //             int temp = arr[j];
-        //             arr[j] = arr[j + 1];
-        //             arr[j + 1] = temp;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
-        //             isSorted = false;
-        //         }
-        //     }
+                    isSorted = false;
+                }
+            }
 
-        //     // If no swaps happened → already sorted
-        //     if (isSorted)
-        //         break;
-        // }
+            // If no swaps happened → already sorted
+            if (isSorted)
+                break;
+        }
 
 
         // Bubble sorting in descending order
@@ -48,21 +49,21 @@ public class Bubble_Sort {
         // }
 
         // Move all zeros to end
-        for(int i=0; i<n-1; i++){
-            boolean isSorted = true;
-            for(int j=0; j<n-1-i; j++){
-                      if(arr[j]==0 && arr[j+1] != 0){
-                        int temp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = temp;
+        // for(int i=0; i<n-1; i++){
+        //     boolean isSorted = true;
+        //     for(int j=0; j<n-1-i; j++){
+        //               if(arr[j]==0 && arr[j+1] != 0){
+        //                 int temp = arr[j];
+        //                 arr[j] = arr[j+1];
+        //                 arr[j+1] = temp;
 
-                        isSorted = false;
-                      }
-            }
-            if (isSorted == true) {
-                break;
-            }
-        }
+        //                 isSorted = false;
+        //               }
+        //     }
+        //     if (isSorted == true) {
+        //         break;
+        //     }
+        // }
 
         System.out.println();
         print(arr);
--- Bubble_Selection_Insertion_Sort/Selection_Sorting.java
+++ Bubble_Selection_Insertion_Sort/Selection_Sorting.java
@@ -5,71 +5,73 @@ import java.util.Scanner;
 
 public class Selection_Sorting {
     public static void main(String[] args) {
-        int[] arr = { 8, -9, 0, 6, 3, 5, -8 };
+        // int[] arr = { 8, -9, 0, 6, 3, 5, -8 };
+        int[] arr = {7,3,4,7,8,1};
         System.out.println("Enter target value");
         Scanner sc = new Scanner(System.in);
         int target = sc.nextInt();
 
         print(arr, target);
 
-        // for (int ele : arr) {
-        //     System.out.print(ele + " ");
-        // }
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
 
     }
 
+    // Selection sort is unstable as compared to Bubble sort
     public static void print(int[] arr, int target) {
         int n = arr.length;
 
         // Sorting array using selection sort
-        // for (int i = 0; i < n - 1; i++) {
-        //     int min = Integer.MAX_VALUE;
-        //     int mindx = -1;
+        for (int i = 0; i < n - 1; i++) {
+            int min = Integer.MAX_VALUE;
+            int mindx = -1;
 
-        //     for (int j = i; j < n; j++) {
-        //         if (arr[j] < min) {
-        //             min = arr[j];
-        //             mindx = j;
-        //         }
-        //     }
+            for (int j = i; j < n; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    mindx = j;
+                }
+            }
 
-        //     // swap
-        //     int temp = arr[i];
-        //     arr[i] = arr[mindx];
-        //     arr[mindx] = temp;
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[mindx];
+            arr[mindx] = temp;
 
-        // }
+        }
 
         // Two sum with time complexity - O(n * log n)
 
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
         
-        int start = 0;
-        int end = n-1;
+        // int start = 0;
+        // int end = n-1;
 
-        boolean found = false;
-        while(start<end){
-               if(arr[start] + arr[end] > target){
-                end--;
+        // boolean found = false;
+        // while(start<end){
+        //        if(arr[start] + arr[end] > target){
+        //         end--;
 
-               }
-               else if (arr[start] + arr[end] < target) {
-                start++;
+        //        }
+        //        else if (arr[start] + arr[end] < target) {
+        //         start++;
                 
-               }
-               else if(arr[start] + arr[end] == target){
-                System.out.println("Target achieved by :" + arr[start] + " and "+ arr[end]);
-                found = true;
-                break;
-               }
+        //        }
+        //        else if(arr[start] + arr[end] == target){
+        //         System.out.println("Target achieved by :" + arr[start] + " and "+ arr[end]);
+        //         found = true;
+        //         break;
+        //        }
            
              
-        }
-        if(!found){
-            System.out.println("No pair found");
-        }
+        // }
+        // if(!found){
+        //     System.out.println("No pair found");
+        // }
 
         // Common Elements
-
+        // Kth smallest
     }
 }
