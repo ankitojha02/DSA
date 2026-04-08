27dc57d2ff231d3e6eed8d8ea4b651393d6d3293
ankitojha02 <ojhaa2207@gmail.com>
2026-04-09 00:25:43 +0530

    Basic template of binary search

--- Binary_Search/basics.java
+++ Binary_Search/basics.java
@@ -1,7 +1,40 @@
 package Binary_Search;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class basics {
     public static void main(String[] args) {
-        
+        int[] arr = {10, -89, 46, 90, 45, 38, -81, 90};
+        Arrays.sort(arr);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you want to search");
+        int target = sc.nextInt();
+        int n = arr.length;
+        int low = 0, high = n-1;
+        boolean isAvailable = false;
+
+        while(low<=high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid]> target){
+                high = mid -1;
+            }
+            else if(arr[mid]<target){
+                low = mid + 1;
+            }
+            else{
+                System.out.println("Yes this number is in the array");
+                isAvailable = true;
+                break;
+            }
+        }
+
+        if(!isAvailable){
+            System.out.println("The number you entered is not in the list");
+        }
+
+        sc.close();
     }
 }
