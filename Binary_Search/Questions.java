74cc0baf3b9222956bc5f58bbea05439f33cf18a
ankitojha02 <ojhaa2207@gmail.com>
2026-04-09 18:21:47 +0530

    First occurence question using binary serach

---
+++ Binary_Search/Questions.java
@@ -0,0 +1,42 @@
+package Binary_Search;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Questions {
+
+    // First Occurence Question
+    public static void main(String[] args) {
+        int[] arr = {20, 35, 35, 47, 90, 90, 90, 47, 90, 43};
+        Arrays.sort(arr);
+        int n = arr.length;
+ 
+        int index = -1;
+        System.out.println("Enter your target element");
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        int low = 0, high = n-1;
+
+        while(low<=high){
+            int mid = (high + low)/2;
+            if (arr[mid] < target) {
+                low = mid + 1;
+                
+            }
+            else if(arr[mid] > target){
+                high = mid -1;
+            }
+            else {
+                index = mid; // store possible answer
+                high = mid -1; // move left for first occurence
+            }
+
+        }
+
+        if (index != -1) {
+            System.out.println("First occurrence found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
