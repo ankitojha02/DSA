57fe0124e7c160bb0a8e1b4ab4133415bb45680b
ankitojha02 <ojhaa2207@gmail.com>
2026-04-21 18:15:07 +0530

    last occurence - array binary search

--- Binary_Search/Questions.java
+++ Binary_Search/Questions.java
@@ -7,43 +7,43 @@ public class Questions {
 
     // First Occurence Question
     public static void main(String[] args) {
-        int[] arr = {20, 35, 35, 47, 90, 90, 90, 47, 90, 43};
-        Arrays.sort(arr);
-        int n = arr.length;
+        // int[] arr = {20, 35, 35, 47, 90, 90, 90, 47, 90, 43};
+        // Arrays.sort(arr);
+        // int n = arr.length;
  
-        int index = -1;
-        System.out.println("Enter your target element");
-        Scanner sc = new Scanner(System.in);
-        int target = sc.nextInt();
-        int low = 0, high = n-1;
+        // int index = -1;
+        // System.out.println("Enter your target element");
+        // Scanner sc = new Scanner(System.in);
+        // int target = sc.nextInt();
+        // int low = 0, high = n-1;
 
-        while(low<=high){
-            int mid = (high + low)/2;
-            if (arr[mid] < target) {
-                low = mid + 1;
+        // while(low<=high){
+        //     int mid = (high + low)/2;
+        //     if (arr[mid] < target) {
+        //         low = mid + 1;
                 
-            }
-            else if(arr[mid] > target){
-                high = mid -1;
-            }
-            else {
-                index = mid; // store possible answer
-                high = mid -1; // move left for first occurence
-            }
+        //     }
+        //     else if(arr[mid] > target){
+        //         high = mid -1;
+        //     }
+        //     else {
+        //         index = mid; // store possible answer
+        //         high = mid -1; // move left for first occurence
+        //     }
 
-        }
+        // }
 
-        if (index != -1) {
-            System.out.println("First occurrence found at index: " + index);
-        } else {
-            System.out.println("Element not found");
-        }
+        // if (index != -1) {
+        //     System.out.println("First occurrence found at index: " + index);
+        // } else {
+        //     System.out.println("Element not found");
+        // }
 
 
         // Last Occurence
 
         // Binary search in descending order
 
-        
+        // Sorted array search , find if k is present in an array or not
     }
 }
