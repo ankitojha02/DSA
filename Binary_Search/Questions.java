67ef2bbe7017588d64de5504ccd1888c4e3cc196
ankitojha02 <ojhaa2207@gmail.com>
2026-04-22 13:41:55 +0530

    search in descending sorted array

--- Binary_Search/Questions.java
+++ Binary_Search/Questions.java
@@ -47,7 +47,10 @@ public class Questions {
         // Sorted array search , find if k is present in an array or not
     
         // Last Occurence
-        int arr[] = {23, 17, 29, 90, 46, 74, 29, 67, 29, 80, 90};
+        // int arr[] = {23, 17, 29, 90, 46, 74, 29, 67, 29, 80, 90};
+
+        // Mountain array
+        int arr[] = {-1, 0, 1, 2, 5, 7, 8, 6, 3, 2};
         int n = arr.length;
 
         Scanner sc = new Scanner(System.in);
@@ -59,29 +62,54 @@ public class Questions {
     }
 
     public static void binary(int[] arr, int n, int target){
-        Arrays.sort(arr);
-        int low = 0, high = n-1;
-        int index = -1;
-        while(low <= high){
-            int mid = (high + low) / 2;
+        // Arrays.sort(arr);
+        // int low = 0, high = n-1;
+        // int index = -1;
+        // while(low <= high){
+        //     int mid = (high + low) / 2;
 
-            if(arr[mid] < target){
+        //     if(arr[mid] < target){
+        //         low = mid + 1;
+        //     }
+        //     else if(arr[mid] > target){
+        //         high = mid - 1;
+        //     }
+        //     else{
+        //         index = mid;
+        //         low = mid + 1;
+        //     }
+        // }
+
+        // if(index == -1){
+        //     System.out.println("The number is not present in the array");
+        // }
+        // else{
+        //     System.out.println("The number is present at index : " + index);
+        // }
+
+        // Search in mountain array
+        int peak = -1;
+        for(int i=1; i<n-1; i++){
+            if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+                peak= i;
+                System.out.println("Peak element is : "+ arr[i] + " at index : "+ peak);
+            }
+        }
+        int low = 0, high = peak ;
+
+        while(low<=high){
+            int mid = (high + low)/2;
+
+            if(arr[mid]<target){
                 low = mid + 1;
             }
             else if(arr[mid] > target){
                 high = mid - 1;
             }
             else{
-                index = mid;
-                low = mid + 1;
+                System.out.println("Target element found at index: "+ mid);
+                break;
             }
-        }
-
-        if(index == -1){
-            System.out.println("The number is not present in the array");
-        }
-        else{
-            System.out.println("The number is present at index : " + index);
         }
     }
 }
