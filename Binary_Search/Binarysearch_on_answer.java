89d4790c0b40874ac0fef834d2ee285b8300264c
ankitojha02 <ojhaa2207@gmail.com>
2026-04-28 12:47:49 +0530

    capacity to ship packages within D days

--- Binary_Search/Binarysearch_on_answer.java
+++ Binary_Search/Binarysearch_on_answer.java
@@ -2,6 +2,8 @@ package Binary_Search;
 
 import java.util.Scanner;
 
+
+
 public class Binarysearch_on_answer {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -15,8 +17,45 @@ public class Binarysearch_on_answer {
         search(arr, n, D);
     }
 
-    public static void search(int[] arr, int n, int D){
+    public static void search(int[] weights, int n, int D){
         int sum = 0, maxWeight = 0;
-        
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            sum = sum + weights[i];
+            maxWeight = Math.max(maxWeight, weights[i]);
+        }
+
+        int low = maxWeight, high = sum;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+
+            if(isShip(weights, mid, D)){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        System.out.println("The minimum capacity is : " + ans);
+    }
+
+    public static boolean isShip(int[] weights, int capacity, int D){
+        int daysRequired = 1;
+        int currentload = 0;
+
+        for(int i=0; i<weights.length; i++){
+            if (currentload + weights[i] > capacity) {
+                daysRequired++;
+                currentload = weights[i];
+            }
+            else{
+                currentload = currentload + weights[i];
+            }
+        }
+
+        return daysRequired <= D;
     }
 }
