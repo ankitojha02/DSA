a2f0583b4d497c16b21fcc8b145ce9c05670e102
ankitojha02 <ojhaa2207@gmail.com>
2026-04-28 12:38:42 +0530

    Valid triangle number

--- Binary_Search/Binarysearch_on_answer.java
+++ Binary_Search/Binarysearch_on_answer.java
@@ -1,14 +1,22 @@
 package Binary_Search;
 
+import java.util.Scanner;
+
 public class Binarysearch_on_answer {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         int n = arr.length;
-        search(arr, n);
+        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of days");
+        int D = sc.nextInt();
+        search(arr, n, D);
     }
 
-    public static void search(int[] arr, int n){
+    public static void search(int[] arr, int n, int D){
+        int sum = 0, maxWeight = 0;
         
     }
 }
