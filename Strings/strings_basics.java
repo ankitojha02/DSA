1c833c21288e2cd1546a3428acfd911dcefb4434
ankitojha02 <ojhaa2207@gmail.com>
2026-05-04 22:41:27 +0530

    Intro to strings in java

---
+++ Strings/strings_basics.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class strings_basics {
+    public static void main(String[] args) {
+        String name = "Satyarth is a good boy";
+        System.out.println(name);
+
+        // String Methods
+        // System.out.println(name.length());
+        // System.out.println(name.toUpperCase());
+        // System.out.println(name.toLowerCase());
+        // System.out.println(name.startsWith("Sat"));
+        // System.out.println(name.endsWith("th"));
+        // System.out.println(name.charAt(0));
+        // System.out.println(name.indexOf("a"));
+
+        // for (int i = 0; i < name.length(); i++) {
+        //     System.out.print(name.charAt(i) + " ");
+        // }
+
+        
+    }
+}
