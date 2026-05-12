77804c4c9ed9685d5f2189cc6172f37abaddaf36
ankitojha02 <ojhaa2207@gmail.com>
2026-05-12 23:22:48 +0530

    printing 1 to n

--- Recursion/Basics_recursion.java
+++ Recursion/Basics_recursion.java
@@ -1,17 +1,32 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class Basics_recursion {
     public static void main(String[] args) {
-        print(5);
+        // print(5);
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        print(1, n);
     }
 
-   public static void print(int n) {
-       if(n==0){
-        return;
-       }
-       System.out.println(n);
+//    public static void print(int n) {
+//        if(n==0){
+//         return;
+//        }
+//        System.out.println(n);
 
-       print(n-1);
+//        print(n-1);
 
+//     }
+    
+    public static void print(int i, int n){
+        if(i>n){
+            return;
+        }
+        System.out.println(i);
+        print(i+1, n);
     }
 }
