6e070534c331d2720e79bfb254c90d0394c804f4
ankitojha02 <ojhaa2207@gmail.com>
2026-05-16 15:19:06 +0530

    reverse a number using recursion

--- Recursion/Basics_recursion.java
+++ Recursion/Basics_recursion.java
@@ -11,10 +11,12 @@ public class Basics_recursion {
         Scanner sc = new Scanner(System.in);
     //    int n= sc.nextInt();
         int a = sc.nextInt();
-        int b = sc.nextInt();
+        // int b = sc.nextInt();
         // print(n);
 
-        System.out.println(power(a,b));
+        // System.out.println(power(a,b));
+        // System.out.println(reverse(a, 0));
+          System.out.println(sum(a));
     }
 
 //    public static void print(int n) {
@@ -67,10 +69,43 @@ public class Basics_recursion {
 
     // 'a' raise to power 'b' using recursion
 
-    public static int power(int a, int b){
-        if(b==0){
-            return 1;
+    // public static int power(int a, int b){
+    //     if(b==0){
+    //         return 1;
+    //     }
+    //     return a * power(a, b-1);
+    // }
+
+    // Optimized way to calculate 'a' raise to power 'b' using recursion
+    // public static int power(int a, int b){
+    //     if(b==0){
+    //         return 1;
+    //     }
+    //     int halfPower = power(a, b/2);
+    //     int fullPower = halfPower * halfPower;
+
+    //     if(b%2!=0){
+    //         fullPower *= a;
+    //     }
+    //     return fullPower;
+    // }
+
+    // Reverse a number using recursion
+    // public static int reverse(int n, int rev){
+    // if(n==0){
+    // return rev; 
+    // }
+    // int rem = n%10;
+    // rev = rev*10 + rem;
+    // return reverse(n/10, rev);
+
+    // Sum of natural numbers using recursion
+    public static int sum(int n){
+        if(n==0){
+            return 0;
         }
-        return a * power(a, b-1);
+        return n + sum(n-1);
     }
+
 }
+
