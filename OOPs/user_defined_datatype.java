8aa087b0c52982c39b18e610ad9cce6effaced0b
ankitojha02 <ojhaa2207@gmail.com>
2026-06-06 23:15:48 +0530

    creating class and object

--- OOPs/user_defined_datatype.java
+++ OOPs/user_defined_datatype.java
@@ -1,7 +1,15 @@
 package OOPs;
 
 public class user_defined_datatype {
+    public static class Student { // Khudka data type banaya
+        String name;
+        int age;
+        double cgpa;
+    }
     public static void main(String[] args) {
-        
+        Student student1 = new Student(); // Object banaya
+        student1.name = "John";
+        student1.age = 20;
+        student1.cgpa = 3.5;
     }
 }
