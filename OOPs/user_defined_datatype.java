ef2dfaf214f958d541c9f30e15853aa6300e842b
ankitojha02 <ojhaa2207@gmail.com>
2026-06-06 23:50:56 +0530

    passing object to method

--- OOPs/user_defined_datatype.java
+++ OOPs/user_defined_datatype.java
@@ -11,5 +11,19 @@ public class user_defined_datatype {
         student1.name = "John";
         student1.age = 20;
         student1.cgpa = 3.5;
+
+        Student student2 = new Student(); // Dusra object banaya
+        student2.name = "Alice";
+        student2.age = 22;
+        student2.cgpa = 3.8;
+
+        System.out.println("Student 1: " + student1.name + ", Age: " + student1.age + ", CGPA: " + student1.cgpa);
+        System.out.println("Student 2: " + student2.name + ", Age: " + student2.age + ", CGPA: " + student2.cgpa);
+
+        // Passing object to method
+        printStudentDetails(student1);
+    }
+    public static void printStudentDetails(Student student) {
+        System.out.println("Student Details: " + student.name + ", Age: " + student.age + ", CGPA: " + student.cgpa);
     }
 }
