087d0ae6dbb4aa33a96e467fad932e9a003e5807
ankitojha02 <ojhaa2207@gmail.com>
2026-06-07 23:50:52 +0530

    Intro to constructor

---
+++ OOPs/constructor.java
@@ -0,0 +1,28 @@
+package OOPs;
+
+public class constructor {
+    public static class Car {
+        String brand;
+        String model;
+        int year;
+
+        // Constructor to initialize the Car object
+        Car(String brand, String model, int year) {
+            this.brand = brand;
+            this.model = model;
+            this.year = year;
+        }
+
+        void display() {
+            System.out.println("Brand: " + brand);
+            System.out.println("Model: " + model);
+            System.out.println("Year: " + year);
+        }
+    }
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Camry", 2020);
+        Car car2 = new Car("Honda", "Civic", 2019);
+        car1.display();
+        car2.display();
+    }
+}
