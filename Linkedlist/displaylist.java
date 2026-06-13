9e2ba24f61f13ed815800910e77629a02e0dc87a
ankitojha02 <ojhaa2207@gmail.com>
2026-06-13 14:06:33 +0530

    traversing in a linkedlist

---
+++ Linkedlist/displaylist.java
@@ -0,0 +1,38 @@
+package Linkedlist;
+
+class Node {
+    int val;
+    Node next; // default value of next is null
+    Node(int val) {
+        this.val = val;
+       
+    }
+}
+
+public class displaylist {
+
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // Node banaya or usme value daali
+        Node a = new Node(10); // head node
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50); // tail node
+        
+        // Node ko link kiya
+        a.next = b;
+        b.next = c; 
+        c.next = d;
+        d.next = e;
+
+        display(a);
+    }
+}
