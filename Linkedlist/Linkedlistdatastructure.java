fefd41da8b35479cf933f5e058bb8e2550980eee
ankitojha02 <ojhaa2207@gmail.com>
2026-06-14 20:28:51 +0530

    creating user made methods- addAtEnd, addAtHead, deleteAtHead

--- Linkedlist/Linkedlistdatastructure.java
+++ Linkedlist/Linkedlistdatastructure.java
@@ -14,11 +14,11 @@ class Linkedlist{
     Node tail;
     void addAtEnd(int val){
         Node newNode = new Node(val);
-        if(head == null){
-            head = newNode;
+        if(head == null){ // agar head null hai, to linked list empty hai, to newNode ko head aur tail dono banado
+            head = newNode; 
             tail = newNode;
         }else{
-            tail.next = newNode;
+            tail.next = newNode; // tail ke next me newNode ko link kiya, kyunki newNode ab tail ban jayega, to purana tail ke next me newNode ko link karna padega
             tail = newNode;
         }
     }
@@ -29,10 +29,19 @@ class Linkedlist{
             head = newNode;
             tail = newNode;
         }else{
-            newNode.next = head;
+            newNode.next = head; // newNode ke next me head ko link kiya, kyunki newNode ab head ban jayega, to uske next me purana head ko link karna padega
             head = newNode;
         }
     }
+    
+        void deleteAtHead() {
+            if(head == null){
+                System.out.println("List is empty");
+                return;
+            }
+            head = head.next; // head ke next me head ko link kiya, kyunki head ko delete karna hai, to uske next me jo node hai, usko head banado
+        }
+
     void display(){
         Node temp = head;
         while(temp != null){
@@ -53,6 +62,9 @@ public class Linkedlistdatastructure {
 
         list.display();
         list.addAtHead(50);
+        list.display();
+        
+        list.deleteAtHead();
         list.display();
     }
    
