8415c36c1f3472d9e88f9d5546f90b442f11f6d9
ankitojha02 <ojhaa2207@gmail.com>
2026-06-15 23:07:07 +0530

    understanding time complexity of these methods

--- Linkedlist/Linkedlistdatastructure.java
+++ Linkedlist/Linkedlistdatastructure.java
@@ -12,6 +12,7 @@ class Node {
 class Linkedlist{
     Node head;
     Node tail;
+    int size;
     void addAtEnd(int val){
         Node newNode = new Node(val);
         if(head == null){ // agar head null hai, to linked list empty hai, to newNode ko head aur tail dono banado
@@ -21,6 +22,7 @@ class Linkedlist{
             tail.next = newNode; // tail ke next me newNode ko link kiya, kyunki newNode ab tail ban jayega, to purana tail ke next me newNode ko link karna padega
             tail = newNode;
         }
+        size++;
     }
 
     void addAtHead(int val) {
@@ -32,6 +34,7 @@ class Linkedlist{
             newNode.next = head; // newNode ke next me head ko link kiya, kyunki newNode ab head ban jayega, to uske next me purana head ko link karna padega
             head = newNode;
         }
+        size++;
     }
     
         void deleteAtHead() {
@@ -40,6 +43,8 @@ class Linkedlist{
                 return;
             }
             head = head.next; // head ke next me head ko link kiya, kyunki head ko delete karna hai, to uske next me jo node hai, usko head banado
+            tail = null; // tail ko null kardo, kyunki ab list empty hai
+            size--;
         }
 
     void display(){
@@ -55,7 +60,7 @@ class Linkedlist{
 public class Linkedlistdatastructure {
     public static void main(String[] args) {
         Linkedlist list = new Linkedlist();
-        list.addAtEnd(10);
+        list.addAtEnd(10); // Time complexity of addAtEnd is O(1) because we are maintaining a tail pointer, so we can directly add the new node at the end without traversing the list. If we didn't maintain a tail pointer, the time complexity would be O(n) because we would have to traverse the entire list to find the last node.
         list.addAtEnd(20);
         list.addAtEnd(30);
         list.addAtEnd(40);
@@ -66,6 +71,9 @@ public class Linkedlistdatastructure {
         
         list.deleteAtHead();
         list.display();
+        System.out.println("Size of the list: " + list.size);
+        
+        
     }
    
 }
