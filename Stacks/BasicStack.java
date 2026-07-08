5fd03da8bf0834c4a5272d3511a8bb47879cfb8d
ankitojha02 <ojhaa2207@gmail.com>
2026-07-08 13:08:58 +0530

    Intro to stacks data structure

---
+++ Stacks/BasicStack.java
@@ -0,0 +1,14 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class BasicStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        System.out.println(st);
+    }
+}
