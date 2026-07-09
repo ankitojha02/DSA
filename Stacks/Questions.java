c1e9a3b3657b5a9d850d2b4a7f803d227cdb0e53
ankitojha02 <ojhaa2207@gmail.com>
2026-07-09 11:43:14 +0530

    push element at bottom of the stack - Recursively

---
+++ Stacks/Questions.java
@@ -0,0 +1,28 @@
+package Stacks;
+
+import java.util.Stack;
+public class Questions {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+
+        // Push element at bottom of the stack- Recursively
+         pushAtBottom(st, 5);
+         System.out.println(st);
+    }
+
+    public static void pushAtBottom(Stack<Integer> st, int data) {
+        if(st.isEmpty()) {
+            st.push(data);
+            return;
+        }
+        int top = st.pop();
+        pushAtBottom(st, data);
+        st.push(top);
+    }
+    
+}
