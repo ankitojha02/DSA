107f80b4197e34dd444d9560d8848117a61cbd69
ankitojha02 <ojhaa2207@gmail.com>
2026-07-09 12:33:12 +0530

    Linked list implementation of stack

---
+++ Stacks/LinkedlistImplementationOfStack.java
@@ -0,0 +1,61 @@
+package Stacks;
+
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class MyStack {
+       Node head;
+       int len;
+
+       int peek() {
+           if (head == null) {
+               return -1;
+           }
+           return head.val;
+       }
+
+       int pop() {
+           if (head == null) {
+               return -1;
+           }
+           int top = head.val;
+           head = head.next;
+           len--;
+           return top;
+       }
+
+       void push(int val) {
+        if (head == null) {
+            head = new Node(val);
+            len++;
+            return;
+        }
+           Node newNode = new Node(val);
+           newNode.next = head;
+           head = newNode;
+           len++;
+       }
+
+       int size() {
+           return len;
+       }
+}
+
+public class LinkedlistImplementationOfStack {
+    public static void main(String[] args) {
+        MyStack st = new MyStack();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        System.out.println(st.peek());
+        System.out.println(st.pop());
+        System.out.println(st.size());
+    }
+}
