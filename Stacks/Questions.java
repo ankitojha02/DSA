cad9b7e28c4185d3f8502e10d6e55d4efc1d6f0f
ankitojha02 <ojhaa2207@gmail.com>
2026-07-10 23:51:52 +0530

    parenthesis checker - Using Stack

--- Stacks/Questions.java
+++ Stacks/Questions.java
@@ -40,5 +40,26 @@ public class Questions {
     }
 
      // parenthesis checker
-     
+     public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        if (s.length()%2==1) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char top = st.pop();
+                if ((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+
 }
