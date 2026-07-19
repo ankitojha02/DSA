2ec022af7941afffb2d135f5d7ae8861c72a4645
ankitojha02 <ojhaa2207@gmail.com>
2026-07-19 20:08:54 +0530

    Optimized solution for Stock Span Problem

--- Stacks/Questions.java
+++ Stacks/Questions.java
@@ -164,6 +164,19 @@ public class Questions {
         return result;
     }
 
-    // Stock Span Problem - Leetcode 901
+    // Stock Span Problem - GFG Practice
+    public static int[] stockSpan(int[] prices) {
+        int n = prices.length;
+        int[] span = new int[n];
+        Stack<Integer> st = new Stack<>();
 
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && prices[st.peek()] <= prices[i]) {
+                st.pop();
+            }
+            span[i] = st.isEmpty() ? (i + 1) : (i - st.peek());
+            st.push(i);
+        }
+        return span;
+    }
 }
