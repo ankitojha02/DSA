9d42dbc6ad18d62ce0e3a895bbb5d83bdd822662
ankitojha02 <ojhaa2207@gmail.com>
2026-07-27 23:29:25 +0530

    Queues built-in methods

--- Queues/BasicQueue.java
+++ Queues/BasicQueue.java
@@ -5,5 +5,18 @@ import java.util.Queue;
 public class BasicQueue {
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<>();
+        // Adding elements to the queue
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        System.out.println("Queue: " + queue); // nakli printing the queue
+        // Removing elements from the queue
+        queue.remove();
+        System.out.println("Queue after removing an element: " + queue);
+        System.out.println("Front element: " + queue.peek()); // Getting the front element  
+        System.out.println("Is the queue empty? " + queue.isEmpty()); // Checking if the queue is empty
+        System.out.println("Size of the queue: " + queue.size()); // Getting the size of the queue
+        
     }
 }
