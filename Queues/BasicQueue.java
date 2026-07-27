721409d89be64b144a5e0fd4d7f0b081c179205a
ankitojha02 <ojhaa2207@gmail.com>
2026-07-27 23:25:49 +0530

    Intro to Queue

---
+++ Queues/BasicQueue.java
@@ -0,0 +1,9 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class BasicQueue {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+    }
+}
