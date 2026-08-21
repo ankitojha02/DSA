41d6b73d4d2ea5b59ffdde59c3774ba9f6e3d4c8
ankitojha02 <ojhaa2207@gmail.com>
2026-08-21 23:35:34 +0530

    LeetCode 700 Binary Search Tree

--- Binary_Search_Tree/Basics_BST.java
+++ Binary_Search_Tree/Basics_BST.java
@@ -1,38 +1,38 @@
 package Binary_Search_Tree;
 
 class Node {
-    int key;
+    int val;
     Node left, right;
 
     public Node(int item) {
-        key = item;
+        val = item;
         left = right = null;
     }
 }
 
 public class Basics_BST {
     public static void main(String[] args) {
-        Node a = new Node(10);
-        Node b = new Node(20);
-        Node c = new Node(30);
-        Node d = new Node(40);
-        Node e = new Node(50);
+        Node a = new Node(50);
+        Node b = new Node(30);
+        Node c = new Node(70);
+        Node d = new Node(20);
+        Node e = new Node(40);
         Node f = new Node(60);
-        Node g = new Node(70);
-        Node h = new Node(80);
+        Node g = new Node(80);
+        Node h = new Node(55);
 
         // Binary Search Tree is the data structure that has the following properties:
         // 1. The left subtree of a node contains only nodes with keys less than the node's key.
         // 2. The right subtree of a node contains only nodes with keys greater than the node's key.
         // 3. Both the left and right subtrees must also be binary search trees.
         // Connecting nodes to form a binary search tree
-        //         10
+        //         50
         //       /    \
-        //     20      30
+        //     30      70
         //    /  \    /  \
-        //   40   50 60   70
+        //   20   40 60   80
         //          /    \
-        //         80
+        //         55
         a.left = b;
         a.right = c;
         b.left = d;
@@ -41,5 +41,24 @@ public class Basics_BST {
         c.right = g;
         g.right = h;
 
+       Node result = searchBST(a, 60);
+       if (result != null) {
+       System.out.println(result.val); // Output: 60
+       }
+       else {
+       System.out.println("Value not found in the BST.");
+       }
+
+    }
+
+    // LeetCode 700 : Search in a Binary Search Tree
+    public static Node searchBST(Node root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
     }
 }
