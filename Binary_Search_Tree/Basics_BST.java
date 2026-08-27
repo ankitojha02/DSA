8081045a5f9cc6b3421251f6452c654b1e520267
ankitojha02 <ojhaa2207@gmail.com>
2026-08-27 23:18:29 +0530

    LeetCode 114 solution using Morris Traversal

--- Binary_Search_Tree/Basics_BST.java
+++ Binary_Search_Tree/Basics_BST.java
@@ -25,17 +25,19 @@ public class Basics_BST {
         Node h = new Node(55);
 
         // Binary Search Tree is the data structure that has the following properties:
-        // 1. The left subtree of a node contains only nodes with keys less than the node's key.
-        // 2. The right subtree of a node contains only nodes with keys greater than the node's key.
+        // 1. The left subtree of a node contains only nodes with keys less than the
+        // node's key.
+        // 2. The right subtree of a node contains only nodes with keys greater than the
+        // node's key.
         // 3. Both the left and right subtrees must also be binary search trees.
         // Connecting nodes to form a binary search tree
-        //         50
-        //       /    \
-        //     30      70
-        //    /  \    /  \
-        //   20   40 60   80
-        //          /    \
-        //         55
+        // 50
+        // / \
+        // 30 70
+        // / \ / \
+        // 20 40 60 80
+        // / \
+        // 55
         a.left = b;
         a.right = c;
         b.left = d;
@@ -44,18 +46,20 @@ public class Basics_BST {
         c.right = g;
         g.right = h;
 
-       Node result = searchBST(a, 60);
-       if (result != null) {
-       System.out.println(result.val); // Output: 60
-       }
-       else {
-       System.out.println("Value not found in the BST.");
-       }
+        Node result = searchBST(a, 60);
+        if (result != null) {
+            System.out.println(result.val); // Output: 60
+        } else {
+            System.out.println("Value not found in the BST.");
+        }
 
     }
 
     // LeetCode 700 : Search in a Binary Search Tree
-    // Time Complexity: O(h), where h is the height of the tree. In the worst case, the height of the tree can be equal to the number of nodes in the tree (O(n)), making the time complexity O(n). In a balanced BST, the height is log(n), making the time complexity O(log n).
+    // Time Complexity: O(h), where h is the height of the tree. In the worst case,
+    // the height of the tree can be equal to the number of nodes in the tree
+    // (O(n)), making the time complexity O(n). In a balanced BST, the height is
+    // log(n), making the time complexity O(log n).
     public static Node searchBST(Node root, int val) {
         if (root == null || root.val == val) {
             return root;
@@ -97,16 +101,25 @@ public class Basics_BST {
     }
 
     // LeetCode 701 - Insert into a Binary Search Tree
-    // Time Complexity: O(h), where h is the height of the tree. In the worst case, the height of the tree can be equal to the number of nodes in the tree (O(n)), making the time complexity O(n). In a balanced BST, the height is log(n), making the time complexity O(log n).
+    // Time Complexity: O(h), where h is the height of the tree. In the worst case,
+    // the height of the tree can be equal to the number of nodes in the tree
+    // (O(n)), making the time complexity O(n). In a balanced BST, the height is
+    // log(n), making the time complexity O(log n).
     public static Node insertIntoBST(Node root, int val) {
 
         if (root == null) {
             return new Node(val);
         }
-        if (val < root.val) { // If the value to be inserted is less than the current node's value, we go to the left subtree
-            root.left = insertIntoBST(root.left, val); // Recursively call the function on the left subtree and assign the returned node to the left child of the current node
+        if (val < root.val) { // If the value to be inserted is less than the current node's value, we go to
+                              // the left subtree
+            root.left = insertIntoBST(root.left, val); // Recursively call the function on the left subtree and assign
+                                                       // the returned node to the left child of the current node
         } else {
-            root.right = insertIntoBST(root.right, val); // If the value to be inserted is greater than or equal to the current node's value, we go to the right subtree and recursively call the function on the right subtree and assign the returned node to the right child of the current node
+            root.right = insertIntoBST(root.right, val); // If the value to be inserted is greater than or equal to the
+                                                         // current node's value, we go to the right subtree and
+                                                         // recursively call the function on the right subtree and
+                                                         // assign the returned node to the right child of the current
+                                                         // node
         }
         return root;
     }
@@ -115,9 +128,9 @@ public class Basics_BST {
     // Time Complexity: O(n), where n is the number of nodes in the tree.
     // LeetCode 230 - Kth Smallest Element in a BST
     public static int kthSmallest(Node root, int k) {
-       ArrayList<Integer> arr = new ArrayList<>();
-         inorderTraversal(root, arr);
-          return arr.get(k - 1);    
+        ArrayList<Integer> arr = new ArrayList<>();
+        inorderTraversal(root, arr);
+        return arr.get(k - 1);
     }
 
     public static void inorderTraversal(Node root, ArrayList<Integer> arr) {
@@ -171,21 +184,21 @@ public class Basics_BST {
     // LeetCode 1373 - Maximum Sum BST in Binary Tree - Solution on LeetCode
     // Time Complexity: O(n), where n is the number of nodes in the tree.
 
-  
-   // LeetCode 235 - Lowest Common Ancestor of a Binary Search Tree
-   // Time Complexity: O(h), where h is the height of the tree. In the worst case, the height of the tree can be equal to the number of nodes in the tree.
+    // LeetCode 235 - Lowest Common Ancestor of a Binary Search Tree
+    // Time Complexity: O(h), where h is the height of the tree. In the worst case,
+    // the height of the tree can be equal to the number of nodes in the tree.
     public static Node lowestCommonAncestor(Node root, Node p, Node q) {
-          if (root == null) {
-                return null;
-          }
-          if (p.val < root.val && q.val < root.val) {
-                return lowestCommonAncestor(root.left, p, q);
-          }
-          if (p.val > root.val && q.val > root.val) {
-                return lowestCommonAncestor(root.right, p, q);
-          }
-          return root;
-     }
+        if (root == null) {
+            return null;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
 
     // LeetCode 108 - Convert Sorted Array to Binary Search Tree
     public static Node sortedArrayToBST(int[] nums) {
@@ -193,7 +206,8 @@ public class Basics_BST {
     }
 
     public static Node sortedArrayToBSTHelper(int[] nums, int low, int high) {
-        if (low > high) { // Base case: If the low index is greater than the high index, it means there are no elements to process, so we return null.
+        if (low > high) { // Base case: If the low index is greater than the high index, it means there
+                          // are no elements to process, so we return null.
             return null;
         }
         int mid = low + (high - low) / 2;
@@ -205,41 +219,43 @@ public class Basics_BST {
 
     // Inorder Predecessor in BST
     public static Node inorderPredecessor(Node root, Node p) {
-       // Ek baar jaao left subtree mein, aur wahan se rightmost node ko return karo
-       if (p.left != null) {
-           Node curr = p.left;
-           while (curr.right != null) {
-               curr = curr.right;
-           }
-           return curr;
-       }
-       return null;
+        // Ek baar jaao left subtree mein, aur wahan se rightmost node ko return karo
+        if (p.left != null) {
+            Node curr = p.left;
+            while (curr.right != null) {
+                curr = curr.right;
+            }
+            return curr;
+        }
+        return null;
     }
 
     // Introduction to Morris Traversal
     public static void morrisTraversal(Node root) {
         Node curr = root;
         while (curr != null) {
-           if (curr.left != null) {
-            // Find predecessor
-            Node pred = curr.left;
-            while (pred.right != null && pred.right != curr) {
-                pred = pred.right;
-            }
+            if (curr.left != null) {
+                // Find predecessor
+                Node pred = curr.left;
+                while (pred.right != null && pred.right != curr) {
+                    pred = pred.right;
+                }
 
-            if (pred.right == null) {
-                pred.right = curr;
-                curr = curr.left;
+                if (pred.right == null) {
+                    pred.right = curr;
+                    curr = curr.left;
+                } else {
+                    pred.right = null;
+                    System.out.print(curr.val + " ");
+                    curr = curr.right;
+                }
             } else {
-                pred.right = null;
                 System.out.print(curr.val + " ");
                 curr = curr.right;
             }
-           }
-           else {
-                System.out.print(curr.val + " ");
-                curr = curr.right;
-           }
         }
     }
+
+    // You can solve LeetCode 114 Flatten Binary Tree to Linked List using Morris Traversal
+    
 }
