6e60db7e2f40d9aa4fc2d194d9e65c68ef85ad99
ankitojha02 <ojhaa2207@gmail.com>
2026-09-05 23:55:13 +0530

    Adding values, removing values from hashsets

--- Maps/HashSets.java
+++ Maps/HashSets.java
@@ -6,15 +6,24 @@ import java.util.Set;
 public class HashSets {
     public static void main(String[] args) {
         HashSet<Integer> set = new HashSet<>();
-        set.add(1);
-        set.add(2);
+        set.add(10);
+        set.add(22);
         set.add(3);
         set.add(4);
-        System.out.println(set.size());
-        System.out.println(set.contains(1)); // true
+        System.out.println(set.size()); // 4
+        System.out.println(set.contains(10)); // true
         System.out.println(set.contains(5)); // false
 
-        set.remove(4);
-        System.out.println(set.size());
+        set.remove(4); // remove 4 from the set
+        System.out.println(set.size()); // 3
+        System.out.println(set.contains(4)); // false
+        System.out.println(set.contains(2)); // true
+
+        set.add(2); // adding duplicate value creates set with unique values
+        System.out.println(set.size()); // 3
+
+        System.out.println(set); // [1, 2, 3] - random order, not sorted
+
+
     }
 }
