ac1d56d0f9f3ce2e85e8128b811a891f28ec260f
ankitojha02 <ojhaa2207@gmail.com>
2026-09-14 22:38:15 +0530

    LeetCode 138 - Copy list with random pointer

--- Linkedlist/doublylinkedlist.java
+++ Linkedlist/doublylinkedlist.java
@@ -243,4 +243,38 @@
 //     // Time complexity of flatten function is O(n) where n is the number of nodes in the multilevel doubly linked list. This is because we are visiting each node exactly once during the flattening process.
 
     
+ // LeetCode 138 - Copy List with Random Pointer
+    // class Node {
+    //     int val;
+    //     Node next;
+    //     Node random;
+
+    //     public Node(int val) {
+    //         this.val = val;
+    //         this.next = null;
+    //         this.random = null;
+    //     }
+    // }
+
+    // public static Node copyRandomList(Node head) {
+    //     if (head == null) {
+    //         return null;
+    //     }
+
+    //     Map<Node, Node> map = new HashMap<>();
+    //     Node current = head;
+
+    //     while (current != null) {
+    //         map.put(current, new Node(current.val));
+    //         current = current.next;
+    //     }
+
+    //     current = head;
+    //     while (current != null) {
+    //         map.get(current).next = map.get(current.next);
+    //         map.get(current).random = map.get(current.random);
+    //         current = current.next;
+    //     }
+
+    //     return map.get(head);
 // }
