c14c4b730dfcc29074a96fb10dbf4c4686260065
ankitojha02 <ojhaa2207@gmail.com>
2026-09-18 23:29:46 +0530

    Bitwise XOR

--- Bit_Manipulation/Implementation.java
+++ Bit_Manipulation/Implementation.java
@@ -4,5 +4,7 @@ public class Implementation {
     public static void main(String[] arge) {
         // Bitwise AND - Bitwise OR
         System.out.println("Bitwise AND: " + (51 & 42));
+        System.out.println("Bitwise OR: " + (51 | 42));
+        System.out.println("Bitwise XOR: " + (51 ^ 42));
     }
 }
