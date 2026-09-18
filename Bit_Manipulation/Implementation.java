46019c2b13d4037d0ccc105a2a2edc00a030288f
ankitojha02 <ojhaa2207@gmail.com>
2026-09-18 23:27:15 +0530

    Bitwise AND / OR

--- Bit_Manipulation/Implementation.java
+++ Bit_Manipulation/Implementation.java
@@ -2,6 +2,7 @@ package Bit_Manipulation;
 
 public class Implementation {
     public static void main(String[] arge) {
-        
+        // Bitwise AND - Bitwise OR
+        System.out.println("Bitwise AND: " + (51 & 42));
     }
 }
