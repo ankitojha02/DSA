7b9e118db95d5eba4ed78fc6c514d16ce15767a6
ankitojha02 <ojhaa2207@gmail.com>
2026-09-18 23:42:03 +0530

    LeetCode 136 - Single Number

--- Bit_Manipulation/Implementation.java
+++ Bit_Manipulation/Implementation.java
@@ -2,9 +2,20 @@ package Bit_Manipulation;
 
 public class Implementation {
     public static void main(String[] arge) {
-        // Bitwise AND - Bitwise OR
+        // Bitwise AND - Bitwise OR - Bitwise XOR (Exclusive OR)
         System.out.println("Bitwise AND: " + (51 & 42));
         System.out.println("Bitwise OR: " + (51 | 42));
         System.out.println("Bitwise XOR: " + (51 ^ 42));
+
+
+    }
+
+    // LeetCode 136 - Single Number
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
     }
 }
